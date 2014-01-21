e8714cc6202536ee627276e68846fcbd4307a759
Robert J Schaefer <schae234@umn.edu>
2014-01-21 15:04:59 -0600

    Sliding window for VCF Buffer implemented

--- pypeline/tools/vcf_pipeline/VCFBuffer.py
+++ pypeline/tools/vcf_pipeline/VCFBuffer.py
@@ -3,59 +3,98 @@
 import sys
 
 class Variant(object):
-    def __init__(self,*arg,**kwargs):
+    def __init__(self,line):
         pass
 
 class VCFBuffer(object):
     ''' 
         VCF class which allows you to compare variant entries to
-        other entries within a buffer
+        other entries within a buffer.
+
+        [ [], L1  ]
+
+
         '''
-    def __init__(self,filename):
+    def __init__(self,filename,buffer_function = 'max_size'):
+        # Set up the buffer clearing function
+        self.is_buffer_full = getattr(self,buffer_function)
         # Set up class variable
-        self.max_buffer_size = 100
+        self.max_buffer_size = 0
+        self.window_size = 10 # in bp
         # Set up class data structures
         self.header_buffer = []
-        self.upstream = []
-        self.current = []
-        self.downstream = []
+        self.buffer = []
+        self.current = 0
         # Set up some class files
         self.log = sys.stderr 
         self.file = open(filename,'r')
         # Set up the initial buffer
-        self.down_buffer()
+        self.staged = self.nextvar()
+        self.rebuffer()
 
-
-    def down_buffer(self):
-        # Read in a line
-        line = self.file.readline().strip()
-        if line.startswith("#"):
+    def nextvar(self):
+        ''' return the next variant of the file, handles headers gracefully ''' 
+        line = self.file.readline().strip() 
+        while line.startswith('#'):
             self.header_buffer.append(line)
-        else:
-            if line  == '':
-                fields = []
-            else:
-                fields = line.split()
-            self.downstream.append(fields)
-        # Keep going until we have a full buffer
-        if len(self.downstream) < self.max_buffer_size:
-            self.down_buffer()
-
-
-    def up_buffer(self):
-        while len(self.current) > 1:
-            self.upstream.append(self.current.pop(0))
-        while len(self.upstream) > self.max_buffer_size:
-            self.upstream.pop(0)
+            line = self.file.readline().strip() 
+        return line.split() 
  
+    # Buffer Full functions
+    def max_size(self,stream):
+        ''' The buffer contains a maximum number of SNPs '''
+        if stream == 'downstream':
+            if len(self.buffer) - self.current <= self.max_buffer_size:
+                return False
+            else:
+                return True
+        elif stream == 'upstream':
+            if self.current > self.max_buffer_size:
+                return False
+            else:
+                return True
+            
+    def max_window(self,stream):
+        ''' The buffer contains a maximum number of SNPs within a window ''' 
+        if stream == 'downstream':
+            if self.staged != [] and abs(int(self.staged[1])-int(self.buffer[self.current][1])) <= self.window_size:
+                return False
+            else:
+                return True
+        if stream == 'upstream':
+            if abs(int(self.buffer[0][1])-int(self.buffer[self.current][1])) > self.window_size:
+                return False
+            else:
+                return True
+
+    def rebuffer(self):
+        ''' adjusts buffer so that it satisfies the buffer function ''' 
+        if len(self.buffer) == 0: # Beginning condition
+            return
+        while not self.is_buffer_full('downstream'):
+            self.buffer.append(self.staged)
+            self.staged = self.nextvar()
+        while not self.is_buffer_full('upstream'):
+            self.buffer.pop(0)
+            self.current -= 1
+
     def readline(self):
-        # Take care of the downstream buffer
-        self.down_buffer()
-        # Take care of the current buffer
-        self.current.append(self.downstream.pop(0))
-        # Take care of the upstream buffer
-        self.up_buffer()
-        return self.current[0]
+        ''' returns the next variant in the buffer, handles rebuffering '''
+        # Beginning condition
+        if len(self.buffer) == 0:
+            self.buffer.append(self.staged)
+            self.staged = self.nextvar()
+        elif self.current == len(self.buffer)-1:
+            # End condition
+            if self.staged == []:
+                return self.staged
+            self.buffer.append(self.staged)
+            self.staged = self.nextvar()
+            self.current += 1 # go to next in buffer
+        else:        
+            self.current += 1 # go to next in buffer
+        self.rebuffer()
+        return self.buffer[self.current] 
 
     def header(self):
         return "\n".join(self.header_buffer)
@@ -63,24 +102,19 @@ class VCFBuffer(object):
     def __iter__(self):
         return self
 
-
     def next(self):
         if self.readline() == []:
             raise StopIteration
-        return self.current[0]
+        return self.buffer[self.current]
 
 
     def __str__(self):
-        return '''
-        Upstream:
-            {}
-        Current:
-            {}
-        Downstream:
-            {}
-        '''.format(
-            "\n\t\t".join([",".join(i) for i in self.upstream]),
-            "\n\t\t".join([",".join(i) for i in self.current]),
-            "\n\t\t".join([",".join(i) for i in self.downstream]),
-        )
+        s = '''Buffer:\n-------\n'''
+        for x,y in enumerate(self.buffer):
+            if x == self.current:
+                s+=str(self.buffer[x][0:3])+"<-- Current\n"
+            else:
+                s+=str(self.buffer[x][0:3])+"\n"
+        s+='''Staged:\n-------\n{}'''.format(self.staged[0:3])
+        return s
  
